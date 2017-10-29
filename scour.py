f32cf1f453dbf7563aeff2aee426292f13322cf7
LSaldyt <lucassaldyt@gmail.com>
2017-10-28 20:50:32 -0700

    Adds topic stack

--- scour.py
+++ scour.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+import argparse
 import sys
+import pickle
 
 from project import Project
 
@@ -33,9 +35,27 @@ def get_site_restriction(filename):
     return '({})'.format(' OR '.join(sites))
 '''
 
-def main(args):
-    project = Project()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("subcommand", type=str,
+                        help="Select a subcommand of scour to run")
+    parser.add_argument('subargs', type=str, nargs='*',
+                        help='Sub arguments fed into a sub command')
+    args = parser.parse_args()
+
+    try:
+        with open('.project.pkl', 'rb') as infile:
+            project = pickle.load(infile)
+    except:
+        project = Project()
+
+    project_command = getattr(project, args.subcommand)
+    project_command(*args.subargs)
+
+    with open('.project.pkl', 'wb') as outfile:
+        pickle.dump(project, outfile)
+
     return 0
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv[1:]))
+    sys.exit(main())
